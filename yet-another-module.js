149bd321f89ad30e530baa3aa3e20b2f58997cab
Joshua Hatcher <jhatcher@abacus.com>
2019-03-25 13:59:35 -0400

    SOMETHING: Add comment pertaining to something project in file that has nothing to do with something project

--- yet-another-module.js
+++ yet-another-module.js
@@ -1,5 +1,7 @@
 const yetAnotherModule = {};
 
+// Comment pertaining to the something project...
+
 yetAnotherModule.someFunc = function() {
   console.log('This module will have absolutely no connection to the something module');
 }
