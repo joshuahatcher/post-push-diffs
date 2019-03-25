20279e3fe01996e029f9609fe3767f91b62c535f
Joshua Hatcher <jhatcher@abacus.com>
2019-03-25 12:44:54 -0400

    Add function to some other module that has nothing to do with the something module

--- something-else.js
+++ something-else.js
@@ -1,3 +1,7 @@
 const someOtherModule = {};
 
+someOtherModule.someFunc = function() {
+  console.log('This function is unrelated to the someModule module...');
+}
+
 module.exports = someOtherModule;
