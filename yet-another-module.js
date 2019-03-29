61c0f6251c153b5e6ff5b014ad1f2d28fcbe1911
Joshua Hatcher <jhatcher@abacus.com>
2019-03-29 15:05:16 -0400

    SOMETHING: commit number six

--- yet-another-module.js
+++ yet-another-module.js
@@ -2,6 +2,8 @@ const yetAnotherModule = {};
 
 // Comment pertaining to the something project...
 
+// This comment should get into the diff
+
 yetAnotherModule.someFunc = function() {
   console.log('This module will have absolutely no connection to the something module');
 }
